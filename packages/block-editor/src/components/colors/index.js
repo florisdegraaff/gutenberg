eec55e6bcc16b4ac6bc2155acd4cfc317d930db4
retrofox <rdsuarez@gmail.com>
2020-02-03 20:15:55 -0800

    block-editor: expose getColorObjectByColorSlug

--- packages/block-editor/src/components/colors/index.js
+++ packages/block-editor/src/components/colors/index.js
@@ -2,6 +2,7 @@ export {
 	getColorClassName,
 	getColorObjectByAttributeValues,
 	getColorObjectByColorValue,
+	getColorObjectByColorSlug,
 } from './utils';
 export { createCustomColorsHOC, default as withColors } from './with-colors';
 export { default as __experimentalUseColors } from './use-colors';
